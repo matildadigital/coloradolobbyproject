bcef609a13a618b8afd33d1c2cf7c9f2523c5a2e
matildaasuzu <matilda.asuzu@occipital.com>
2017-12-16 12:43:02 -0700

    Setting up files for lobby clients visualization.

---
+++ source/index.js
@@ -0,0 +1,9 @@
+debugger;
+
+// var mydata = JSON.parse(data);
+//  alert(mydata[0].name);
+//  alert(mydata[0].age);
+//  alert(mydata[1].name);
+//  alert(mydata[1].age);
+// 
+// console.log("lobbyclients");
